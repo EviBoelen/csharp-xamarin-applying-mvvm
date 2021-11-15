66617d42422ca3164e6cb47ff5a76fa40cc26485
EviBoelen <evi.boelen@student.pxl.be>
2021-11-15 13:24:32 +0100

    test

--- Roster.Client/Views/HomeView.xaml.cs
+++ Roster.Client/Views/HomeView.xaml.cs
@@ -5,9 +5,14 @@ namespace Roster.Client.Views
     [System.ComponentModel.DesignTimeVisible(false)]
     public partial class HomeView : ContentPage
     {
+        Label label;
         public HomeView()
         {
             InitializeComponent();
+
+            BindingContext = this.Title = "Roster App";
+            label = new Label();
+            label.SetBinding<HomeView>(Label.TextProperty, hv => hv.Title, mode: BindingMode.OneWay);
         }
     }
 }

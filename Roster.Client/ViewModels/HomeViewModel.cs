4848ae681ce396ac9896921d23a49b152e261935
EviBoelen <evi.boelen@student.pxl.be>
2021-11-15 13:42:05 +0100

    qzd

---
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Roster.Client.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        public string Title { get; } = "Roster App";
+        public Command UpdateApplicationCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        // The constructor is private to enforce the factory pattern.  
+        private HomeViewModel()
+        {
+            customerNameValue = "Customer";
+            phoneNumberValue = "(312)555-0100";
+        }
+
+        // This is the public factory method.  
+        // This property represents an ID, suitable  
+        // for use as a primary key in a database.  
+        public Guid ID
+        {
+            get
+            {
+                return this.idValue;
+            }
+        }
+
+        public string CustomerName
+        {
+            get
+            {
+                return this.customerNameValue;
+            }
+
+            set
+            {
+                if (value != this.customerNameValue)
+                {
+                    this.customerNameValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return this.phoneNumberValue;
+            }
+
+            set
+            {
+                if (value != this.phoneNumberValue)
+                {
+                    this.phoneNumberValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}

e4d69d602758c14845bae61e262d098aa38addbc
EviBoelen <evi.boelen@student.pxl.be>
2021-11-15 14:07:33 +0100

    zdqd

---
+++ Roster.Client/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Roster.Client.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+
+        public ObservableCollection<Person> People { get; set; }
+    }
+}
--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Roster.Client.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -10,6 +12,7 @@ namespace Roster.Client.ViewModels
     public class HomeViewModel : INotifyPropertyChanged
     {
         private string _title = "Roster App";
+        public Person people;
         public string Title
         {
             get { return _title; }
@@ -26,6 +29,7 @@ namespace Roster.Client.ViewModels
         public HomeViewModel()
         {
             UpdateApplicationCommand = new Command(UpdateTitle);
+            FillPeopleCollection();
         }
 
         private void UpdateTitle()
@@ -33,6 +37,15 @@ namespace Roster.Client.ViewModels
             Title = "Roster App (v2.0)";
         }
 
+        private void FillPeopleCollection()
+        {
+            people.People = new ObservableCollection<Person>()
+            {
+                new Person { Name = "Delores Feil", Company = "Legros Group" },
+                new Person { Name = "Ann Zboncak", Company = "Ledner - Ferry" },
+                new Person { Name = "Jaime Lesch", Company = "Herzog and Sons" }
+            };
+        }
         //private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         //{
         //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));

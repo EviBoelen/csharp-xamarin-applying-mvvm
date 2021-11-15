949b850604d87f50ae6f694bf14a39405b8a1d24
EviBoelen <evi.boelen@student.pxl.be>
2021-11-15 13:44:14 +0100

    sdqz

--- Roster.Client/Views/HomeView.xaml.cs
+++ Roster.Client/Views/HomeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using Roster.Client.ViewModels;
+using Xamarin.Forms;
 
 namespace Roster.Client.Views
 {
@@ -9,8 +10,7 @@ namespace Roster.Client.Views
         public HomeView()
         {
             InitializeComponent();
-            BindingContext = new { Title = "Roster App" };
-            Title = "Roster App";
+            BindingContext = new HomeViewModel();
         }
     }
 }
